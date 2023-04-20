852d3c5f38a1ad4c6389c227684c5f4f073c9ba0
Karina Ribeiro <karina.rodrigues.ribeiro@outlook.com>
2023-04-19 22:52:43 -0300

    Project of landing page of event CCXP

---
+++ main.js
@@ -0,0 +1,31 @@
+const dia = document.getElementById('dia')
+const hora = document.getElementById('hora')
+const minuto = document.getElementById('minuto')
+const segundo = document.getElementById('segundo')
+
+const event = "01 dec 2023"
+
+function countDown(){
+  const dataEvent = new Date(event)
+  const hoje = new Date()
+
+  const segTotal = (dataEvent - hoje)/1000;
+
+  const finalDias = Math.floor(segTotal / 60 / 60 / 24);
+  const finalHoras = Math.floor(segTotal / 60 / 60) % 24;
+  const finalMinutos = Math.floor(segTotal / 60) % 60;
+  const finalSegundos = Math.floor(segTotal) % 60;
+
+  dia.innerHTML = formatoTempo(`${finalDias}D`)
+  hora.innerHTML = formatoTempo(`${finalHoras}H`)
+  minuto.innerHTML = formatoTempo(`${finalMinutos}M`)
+  segundo.innerHTML = formatoTempo(`${finalSegundos}S`)
+
+}
+
+function formatoTempo( tempo ){
+  return tempo < 10? `0${tempo}` : tempo;
+}
+
+countDown();
+setInterval(countDown, 1000)
